4be56f2ed825add4803278fd3dcfdd9e8942c16c
Mikkel Kirkegaard Thomsen <mikkelodeon@Mikkels-MacBook-Pro.local>
2024-11-18 09:51:35 +0100

    -

--- fano_theory.py
+++ fano_theory.py
@@ -596,8 +596,8 @@ def line_width_comparison(params1: list, params2: list, length: float, intracavi
 
 #### double fano transmission as a function of losses ####
 
-Ls = [0.0, 0.02, 0.04, 0.06, 0.08] ## loss factor is NOT equal to actual cavity losses
-loss_factor_scan(params1, Ls, λs, lmin=30)
+#Ls = [0.0, 0.02, 0.04, 0.06, 0.08] ## loss factor is NOT equal to actual cavity losses
+#loss_factor_scan(params1, Ls, λs, lmin=30)
 
 
 #### double fano transmission as a function of detuning #### 
