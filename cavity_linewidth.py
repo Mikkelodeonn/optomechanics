6b656bc18b4cca903fe32a898677de8b3988c28d
Mikkel Kirkegaard Thomsen <mikkelodeon@Mikkels-MacBook-Pro.local>
2025-02-11 16:31:07 +0100

    -

--- cavity_linewidth.py
+++ cavity_linewidth.py
@@ -26,6 +26,12 @@ def lw_fano(l: int, λres: float, L: float, γλ: float, rd: float, Tg: float, T
     δγ = 1/((1/δγc) + (1/δγg)) 
     return δγ 
 
+def double_fano(l: int, λres: float, L: float, γλ: float, rd: float, Tg: float, Tm: float):
+    δγc = (λres**2)/(8*np.pi*l) * (Tg + Tm + L) 
+    δγg = (γλ/(2*(1-rd))) * (Tg + Tm + L) * 0.5
+    δγ = 1/((1/δγc) + (1/δγg)) 
+    return δγ 
+
 #lengths = np.array([900])*1e-6 # cavity lengths
 
 lengths = np.array([21,34,43,59,129,238])*1e-6
@@ -33,12 +39,13 @@ lengths = np.array([21,34,43,59,129,238])*1e-6
 #    linewidth = 2*lw_fano(length,λres,L,γλ,rd,Tg,Tm)
 #    print("length of fano cavity: ", round(length*1e6,1), "μm", " -> ", "theoretical linewidth: ", round(linewidth*1e12,1), "pm")
 
-lws = np.array([291,380,187,180,129,96])*1e-12 # line widths
+lws = np.array([291,170,187,180,129,96])*1e-12 # line widths
 
 plt.figure(figsize=(10,6))
 
 plt.plot(l*1e6,2*lw_mirror(l,λres,L,Tg,Tm)*1e12, label="broadband cavity")
-plt.plot(l*1e6,2*lw_fano(l,λres,L,γλ,rd,Tg,Tm)*1e12, label="singlefano cavity")
+plt.plot(l*1e6,2*lw_fano(l,λres,L,γλ,rd,Tg,Tm)*1e12, label="single fano cavity")
+plt.plot(l*1e6,2*double_fano(l,λres,L,γλ,rd,Tg,Tm)*1e12, label="double fano cavity")
 plt.plot(lengths*1e6,lws*1e12, "ro", label="measured linewidths (double fano)")
 plt.title("FWHM as a function of cavity length")
 plt.xlabel("Cavity length [μm]")
