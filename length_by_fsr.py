1fca388b773dbbd702ec5fd2f36b8753216fa0ea
Mikkel Kirkegaard Thomsen <mikkelodeon@Mikkels-MacBook-Pro.local>
2025-02-24 15:29:42 +0100

    -

--- length_by_fsr.py
+++ length_by_fsr.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-data = np.loadtxt("/Users/mikkelodeon/optomechanics/Double fano cavity/M3+M5/data/20250220/25um/25fsr.txt")[3:-1]
-PI = np.loadtxt("/Users/mikkelodeon/optomechanics/Double fano cavity/M3+M5/data/20250220/25um/25fsr_PI.txt")[3:-1]
+data = np.loadtxt("/Users/mikkelodeon/optomechanics/Double fano cavity/M3+M5/data/20250207/59um/59fsr.txt")[3:-1]
+PI = np.loadtxt("/Users/mikkelodeon/optomechanics/Double fano cavity/M3+M5/data/20250207/59um/59fsr_PI.txt")[3:-1]
 
 PI0 = PI[0,1]
 data[:,1] = [d/(pi/PI0) for d,pi in zip(data[:,1], PI[:,1])]
@@ -14,7 +14,7 @@ def fabry_perot(λ, r, t, l, φ):
     T = np.abs((t**2)/(1-r**2*np.exp(2j* ((2 * np.pi / λ) * l + φ))))**2
     return T
 
-p0 = [np.sqrt(0.85),np.sqrt(0.1),20e3,np.pi/2]
+p0 = [np.sqrt(0.85), np.sqrt(0.1), 50e3, np.pi/2]
 popt, pcov = curve_fit(fabry_perot, data[:,0], data[:,1], p0=p0, maxfev=1000000)
 xs = np.linspace(data[:,0][0], data[:,0][-1], 10000)
 
